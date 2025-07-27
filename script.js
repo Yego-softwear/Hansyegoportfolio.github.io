2efb2eee62f02b5569215e89b8088793a3faf83c
HANS KIPROP YEGO <76849602+Yego-softwear@users.noreply.github.com>
2025-07-27 18:59:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let current = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+
+setInterval(() => {
+  testimonials[current].classList.remove('active');
+  current = (current + 1) % testimonials.length;
+  testimonials[current].classList.add('active');
+}, 4000);
